34629fb6b0087e7a41484c60735d2aa23b6fc2b5
m.banti <marina.banti@student.uni-tuebingen.de>
2026-04-26 15:52:39 +0200

    add: excercise 1

---
+++ dippid_sender/DIPPID_sender.py
@@ -0,0 +1,42 @@
+import socket
+import time
+import numpy as np
+import random
+import json
+
+IP = '127.0.0.1'
+PORT = 5700
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# simulate accelerometer based on a sinus curve (taken from assignment hint)
+def sim_accelerometer():
+    t = time.time()
+    # calculates accelaration parameter based on time * weight
+    return {
+        "x": float(np.sin(t*1.5)),
+        "y": float(np.sin(t*2.5)),
+        "z": float(np.sin(t*0.75))
+    }
+
+def sim_button():
+    # presses button randomly based on weighted probability 50%
+    return random.choices([0, 1], weights=[0.5, 0.5])[0]
+
+while True:
+    # message could be print directly in a long string like this,
+    # message = '{"accelerometer": {"x": ' + str(x) + ', "y": ' + str(y) + ', "z": ' + str(z) + '}, "button_1": ' + str(button) + '}'
+    # but it looks ugly so I decided to use a python dictionary
+
+    # collect data in dictionary
+    sensor_data = {
+        "accelorometer": sim_accelerometer(),
+        "button_1": sim_button()
+    }
+    # convert dictionary in JSON-string
+    message = json.dumps(sensor_data)
+
+    print(message)
+
+    sock.sendto(message.encode(), (IP, PORT))
+    
+    time.sleep(1)
